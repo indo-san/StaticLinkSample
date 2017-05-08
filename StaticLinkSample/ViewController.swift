076b80325bb57e91e6917334b5602ae72ac2db05
Ryo Nakata <kk.vivivi@gmail.com>
2017-05-08 19:42:05 +0900

    Sample2

--- StaticLinkSample/ViewController.swift
+++ StaticLinkSample/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("sample")
+        print("sample2")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
